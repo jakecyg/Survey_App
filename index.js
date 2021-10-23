d58176c79088faf8066901569978cd1e74234352
jakecyg <43617272+jakecyg@users.noreply.github.com>
2021-10-22 23:44:20 -0500

    fix port

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get("/", (req, res) => {
 // Heroku injects port number as environment variable at runtime when deployed
 // If there's a env var from heroku = i.e. deployed = use that, otherwise i.e. dev = 5000
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
