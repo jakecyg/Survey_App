713afa1e157a3e19e9c927f9e2db485febc472c1
jakecyg <43617272+jakecyg@users.noreply.github.com>
2021-10-22 23:14:04 -0500

    dynamic port configuration for heroku

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
+// Heroku injects port number as environment variable at runtime when deployed
+// If there's a env var from heroku = i.e. deployed = use that, otherwise i.e. dev = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(5000);
