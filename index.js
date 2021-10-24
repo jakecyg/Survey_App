110c8133b46ca6054969e997673a4701737ed805
jakecyg <43617272+jakecyg@users.noreply.github.com>
2021-10-23 22:06:45 -0500

    add passport

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
 });
 
 // Heroku injects port number as environment variable at runtime when deployed
-// If there's a env var from heroku = i.e. deployed = use that, otherwise i.e. dev = 5000
+// If there's a env var from heroku = i.e. deployed = use env var, otherwise i.e. dev = 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
