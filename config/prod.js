5c7dfc8574de89340db9939d7fd2bf867646f7d4
jakecyg <43617272+jakecyg@users.noreply.github.com>
2021-10-31 17:43:11 -0500

    fix proxy bug

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-
   googeClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
   mongoURI: process.env.MONGO_KEY,
-
   cookieKey: process.env.COKKIE_KEY,
 };
