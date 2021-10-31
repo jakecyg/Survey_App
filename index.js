90b378927a7f409d2a4e75f0e624fb4ffe8033bf
jakecyg <43617272+jakecyg@users.noreply.github.com>
2021-10-31 16:28:45 -0500

    return logged in user at google sign in

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const cookieSession = require("cookie-session");
-const passport = "passport";
+const passport = require("passport");
 
 require("./models/user");
 require("./services/passport");
@@ -11,8 +11,6 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-require("./routes/authRoutes")(app);
-
 // setup express to enable brower cookies with specific config
 app.use(
   cookieSession({
@@ -24,6 +22,7 @@ app.use(
 // set passport to use cookies for authentication
 app.use(passport.initialize());
 app.use(passport.session());
+require("./routes/authRoutes")(app);
 
 // Heroku injects port number as environment variable at runtime when deployed
 // If there's a env var from heroku = i.e. deployed = use env var, otherwise i.e. dev = 5000
